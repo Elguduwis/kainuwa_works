069fa1530dc2ccf4aa1005801f8c1fa30997d56d
Elguduwis <elguduwis@gmail.com>
2026-03-26 15:52:39 +0100

    Updated API base URL to works.kainuwa.africa

--- lib/utils/api_config.dart
+++ lib/utils/api_config.dart
@@ -1,7 +1,8 @@
 class ApiConfig {
-  // TODO: Change this to your actual production domain later
-  static const String baseUrl = 'https://yourdomain.com/api';
+  static const String baseUrl = 'https://works.kainuwa.africa/api';
   
+  // Endpoint definitions
   static const String login = '$baseUrl/auth_login.php';
   static const String getProviders = '$baseUrl/get_providers.php';
+  // We will add more endpoints here as we build the PHP APIs
 }
